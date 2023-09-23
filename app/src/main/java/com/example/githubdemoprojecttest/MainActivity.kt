a05ed411b1d9f456a3715981ac323534fcf870da
sk <talking2saurav@gmail.com>
2023-09-23 10:53:25 +0530

    Initial commit updated

--- app/src/main/java/com/example/githubdemoprojecttest/MainActivity.kt
+++ app/src/main/java/com/example/githubdemoprojecttest/MainActivity.kt
@@ -14,30 +14,9 @@ import com.example.githubdemoprojecttest.databinding.DialogWelcomeBinding
 class MainActivity : AppCompatActivity() {
 
     private var binding: ActivityMainBinding? = null
-    private var dialogBinding: DialogWelcomeBinding? = null
-    private var alertDialog: Dialog? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding?.openNextScreen?.setOnClickListener {
-            showDialog()
-        }
     }
 
-    private fun showDialog() {
-        dialogBinding = null
-        dialogBinding =
-            DataBindingUtil.inflate(LayoutInflater.from(this), R.layout.dialog_welcome, null, false)
-        alertDialog = Dialog(this, R.style.animateDialog)
-        alertDialog!!.setContentView(dialogBinding!!.root)
-        dialogBinding!!.textViewWelcome.setOnClickListener {
-            alertDialog!!.dismiss()
-        }
-        alertDialog!!.show()
-        val layoutParams = WindowManager.LayoutParams()
-        layoutParams.copyFrom(alertDialog!!.window!!.attributes)
-        layoutParams.width = WindowManager.LayoutParams.WRAP_CONTENT
-        layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT
-        alertDialog!!.window!!.attributes = layoutParams
-    }
 }
